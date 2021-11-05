1c0986f978f39d3d9da822aa252b55ae01950677
monjhall <tslourenco@wpi.edu>
2021-11-05 10:57:10 -0700

    Fixed CMake build issues. Initial GTest setup.

--- test/test_morse_translator.cpp
+++ test/test_morse_translator.cpp
@@ -1,24 +1,16 @@
-// Bring in my package's API, which is what I'm testing
-#include "foo/foo.h"
-// Bring in gtest
+#include "morse_translator_node/MorseTranslator.h"
+
 #include <gtest/gtest.h>
 
-// Declare a test
-TEST(TestSuite, testCase1)
+TEST(TranslationTest, DogTest)
 {
-<test things here, calling EXPECT_* and/or ASSERT_* macros as needed>
-}
-
-// Declare another test
-TEST(TestSuite, testCase2)
-{
-<test things here, calling EXPECT_* and/or ASSERT_* macros as needed>
+    ASSERT_TRUE(1 == 1);
 }
 
 // Run all the tests that were declared with TEST()
 int main(int argc, char **argv){
-  testing::InitGoogleTest(&argc, argv);
-  ros::init(argc, argv, "tester");
-  ros::NodeHandle nh;
-  return RUN_ALL_TESTS();
+    testing::InitGoogleTest(&argc, argv);
+    ros::init(argc, argv, "tester");
+    ros::NodeHandle nh;
+    return RUN_ALL_TESTS();
 }
