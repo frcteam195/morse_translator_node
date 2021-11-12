2f002d9db3dcb3d79a3ddc5c1a48570fcdaf42c6
monjhall <tslourenco@wpi.edu>
2021-11-12 17:13:38 -0500

    Reverted attempt at changing test entry point.

--- test/test_morse_translator.cpp
+++ test/test_morse_translator.cpp
@@ -8,7 +8,7 @@ TEST(TranslationTest, Alphanumeric_Verification_Test)
     ASSERT_FALSE(verify_alphanumeric("$195.00 for the #1 Dog"));
 }
 
-int test_main(int argc, char **argv)
+int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
     ros::init(argc, argv, "tester");
